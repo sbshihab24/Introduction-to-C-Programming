e05e84caa83bc75883de5e2d32e67ec67624bd30
Mehedi Hasan Shihab <86728905+sbshihab24@users.noreply.github.com>
2025-06-21 03:35:17 +0600

    Add files via upload

---
+++ Practice-2.5&3.5/practice4.c
@@ -0,0 +1,10 @@
+//Floating Point Number
+#include<stdio.h>
+int main()
+{   
+    float n;
+    scanf("%f", &n);
+    printf("%.3f",n);
+
+    return 0;
+}
---
+++ Practice-2.5&3.5/practice5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    {
+        printf("I Love Practice");
+    }
+
+    return 0;
+}
